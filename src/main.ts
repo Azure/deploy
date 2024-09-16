016e1ae07c25663b4007177df90f8322ee3236b2
Lyon Till <158992+ljtill@users.noreply.github.com>
2024-09-16 01:30:50 +0100

    chore: add action template (#1)

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import * as core from '@actions/core'
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  try {
+    // TODO: Implement
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
